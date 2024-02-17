951d782a0032ed59b41b467e4f98888b54b884c4
chernikov-frontend <chernikov-frontend@yandex.ru>
2024-02-17 09:41:47 +0400

    init

---
+++ src/cashback.js
@@ -0,0 +1,15 @@
+const cashback = {
+    regular: {
+        bound: 1000,
+        percent: 1,
+    },
+    silver: {
+        bound: 10000,
+        percent: 2,
+    },
+    gold: {
+        bound: 100000,
+        percent: 5,
+    },
+};
+
